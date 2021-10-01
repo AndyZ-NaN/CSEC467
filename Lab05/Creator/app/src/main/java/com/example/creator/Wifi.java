0b5f25c002ec274690844e3668bb4407beec6c1d
Andy Zhu <jxz3986@g.rit.edu>
2021-10-01 11:37:54 -0400

    Completed step 2, sent data to invoker

--- Lab05/Creator/app/src/main/java/com/example/creator/Wifi.java
+++ Lab05/Creator/app/src/main/java/com/example/creator/Wifi.java
@@ -1,7 +1,10 @@
 package com.example.creator;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -11,9 +14,21 @@ public class Wifi extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String data = intent.getStringExtra("message");
-        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+        WifiManager wm = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wi = wm.getConnectionInfo();
+        String ssid = wi.getSSID();
+        // Send the data instead of a toast containing the using BroadcastManager
+        Intent ssidIntent = new Intent("ssidBroadcast");
+        ssidIntent.putExtra("ssid", ssid);
+        sendBroadcast(ssidIntent);
+
+        return Service.START_NOT_STICKY;
+        /**
+        Intent i = new Intent("get_ssid");
+        Toast.makeText(this, ssid, Toast.LENGTH_LONG).show();
         return Service.START_NOT_STICKY; // Service stops running after return
+        // String data = intent.getStringExtra("message");
+         */
     }
 
     @Override
--- Lab05/Invoker/app/src/main/java/com/example/invoker/MainActivity.java
+++ Lab05/Invoker/app/src/main/java/com/example/invoker/MainActivity.java
@@ -2,22 +2,37 @@ package com.example.invoker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String ssid = intent.getStringExtra("ssid");
+            Toast.makeText(context, ssid, Toast.LENGTH_LONG).show();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        IntentFilter filter = new IntentFilter("ssidBroadcast");
+        registerReceiver(br, filter);
         setContentView(R.layout.activity_main);
 
         Intent i = new Intent();
-        i.putExtra("message", "Hello World");
         i.setComponent(new ComponentName("com.example.creator", "com.example.creator.Wifi"));
+
         startService(i);
+    }
 
-
+    protected void onDestroy() {
+        unregisterReceiver(br);
+        super.onDestroy();
     }
 }
